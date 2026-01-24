70ba8503a7ba086770b9236058eacec335af8bee
freyes0519901 <76229610+freyes0519901@users.noreply.github.com>
2026-01-24 00:42:48 -0300

    Update route.js

--- app/flow/return/app/flow/return/route.js
+++ app/flow/return/app/flow/return/route.js
@@ -1,1 +1,10 @@
+import { redirect } from 'next/navigation';
 
+export async function POST(request) {
+  // Flow envía POST, redirigimos a la página con GET
+  redirect('/flow/return');
+}
+
+export async function GET(request) {
+  redirect('/flow/return');
+}
