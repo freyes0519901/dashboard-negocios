5223b3e8eecbf0c92e5fe68aab4b227fa3f9459c
freyes0519901 <76229610+freyes0519901@users.noreply.github.com>
2026-01-24 00:53:43 -0300

    Delete app/flow/return/route.js

--- app/flow/return/route.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export async function POST(request) {
-  redirect('/flow/return');
-}
-
-export async function GET(request) {
-  redirect('/flow/return');
-}
